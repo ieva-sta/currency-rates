23f3c8cea4662f5f7626e738a8c7807e27309c12
Ieva <ieva.zemnicka@netcore.lv>
2020-02-05 17:20:22 +0200

    Set up models, migrations, controllers, add relationships.

---
+++ app/Models/Rate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Rate extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'currency_id',
+        'price'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+}
