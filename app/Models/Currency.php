9d874c2d507574e252bc3db4d644376595c7c084
Ieva <ieva.zemnicka@netcore.lv>
2020-02-06 11:25:31 +0200

    Show currency rate trend in percentage.

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 class Currency extends Model
 {
@@ -29,6 +30,7 @@ class Currency extends Model
      */
     public function getTrendAttribute(): bool
     {
+        // @todo remove if unused
         $rates = $this->rates()->orderBy('date')->get();
         $latestRate = $rates->last()->price;
 
@@ -36,5 +38,25 @@ class Currency extends Model
         $comparisonRate = $rates->last()->price;
 
         return $latestRate > $comparisonRate;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getTrend(): Collection
+    {
+        $rates = $this->rates()->orderBy('date')->get();
+        $latestRate = $rates->last()->price;
+
+        $rates = $rates->slice(0, -1);
+        $comparisonRate = $rates->last()->price;
+
+        $percentage = (1 - $comparisonRate / $latestRate) * 100;
+        $trend = $percentage > 0;
+
+        return collect([
+            'trend'      => $trend,
+            'percentage' => number_format(abs($percentage), 2)
+        ]);
     }
 }
--- resources/views/currency/index.blade.php
+++ resources/views/currency/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col bg-white shadow">
+            <div class="col-10 bg-white shadow">
                 <table class="table">
                     <tbody>
                     @foreach($currencies as $currency)
@@ -11,13 +11,20 @@
                             <td>{{ $currency->symbol }}</td>
                             <td>{{ $currency->code }}</td>
                             <td>{{ $currency->title }}</td>
-                            <td>{{ $currency->rates->last()->price }}</td>
-                            <td>{{ $currency->trend }}</td>
+                            <td>
+                                {{ $currency->rates->last()->price }}
+                            </td>
+                            <td>
+                                <i class="ml-1 fas fa-caret-{{ $currency->getTrend()['trend'] ? 'up' : 'down' }}"></i>
+                                {{ $currency->getTrend()['percentage'] }} %
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>
                 </table>
-                {{ $currencies->links() }}
+                <div class="d-flex justify-content-center my-3">
+                    {{ $currencies->links() }}
+                </div>
             </div>
         </div>
     </div>
